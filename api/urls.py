449d445ae14f2d68ce9b869d4b2ff8424ac85458
Rachini <rachinihennayaka@gmail.com>
2026-08-24 14:16:02 +0530

    feat(urls): add hello endpoint to API

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from .views import get_hello
 
 urlpatterns = [
+    path('hello/', get_hello, name='get_hello'),
     path('auth/', include('accounts.urls')),
     path('tickets/', include('tickets.urls')),
     path('projects/', include('projects.urls')),
